e8a3dcb62f889fb0c40b7d37873d4234f193c400
BroPro <bronislawdadan@gmail.com>
2018-02-15 11:01:06 +0100

    part6,7

--- mysite2/polls/models.py
+++ mysite2/polls/models.py
@@ -47,3 +47,10 @@ def test_was_published_recently_with_recent_question(self):
     time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
     recent_question = Question(pub_date=time)
     self.assertIs(recent_question.was_published_recently(), True)
+
+def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Published recently?'
